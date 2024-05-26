956dfbb325c722ba6c1aaacd048eb6e6410fb079
Anton Vorobiov <vorobiov.anton@lll.kpi.ua>
2024-05-26 23:50:25 +0300

    Add viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ export const metadata: Metadata = {};
 const Application: React.FC<React.PropsWithChildren> = (props) => {
   return (
     <html lang="en_US" className="w-full h-full p-0 m-0">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
+      </head>
       <body className="w-full h-full p-0 m-0">{props.children}</body>
     </html>
   );
